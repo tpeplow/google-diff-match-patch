0d3ee7763366a08a83243358398a35ffa9e06504
Tom Peplow <thomas.peplow@milliman.com>
2021-10-08 22:39:31 -0700

    added ListTailSegment

---
+++ src/google-diff-match-patch-tests/ListTailSegmentTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DiffMatchPatch;
+using NUnit.Framework;
+
+namespace DiffMatchPatchTests
+{
+    [TestFixture]
+    public class ListTailSegmentTests
+    {
+        List<int> _completeList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _completeList = new List<int>
+            {
+                0,
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+        }
+
+        [Test]
+        public void when_enumerating_segment()
+        {
+            CollectionAssert.AreEqual(_completeList.ToTailSegment(0).ToArray(), new[] { 0, 1, 2, 3, 4, 5 });
+            CollectionAssert.AreEqual(_completeList.ToTailSegment(2).ToArray(), new[] { 2, 3, 4, 5 });
+            CollectionAssert.AreEqual(_completeList.ToTailSegment(_completeList.Count - 1).ToArray(), new[] { 5 });
+            CollectionAssert.AreEqual(_completeList.ToTailSegment(_completeList.Count).ToArray(), Array.Empty<int>());
+        }
+
+        [Test]
+        public void when_reading_list()
+        {
+            Assert.That(_completeList.ToTailSegment(0)[0], Is.EqualTo(0));
+            Assert.That(_completeList.ToTailSegment(0)[1], Is.EqualTo(1));
+            Assert.That(_completeList.ToTailSegment(1)[0], Is.EqualTo(1));
+            Assert.That(_completeList.ToTailSegment(2)[0], Is.EqualTo(2));
+        }
+
+        [Test]
+        public void when_counting()
+        {
+            Assert.That(_completeList.ToTailSegment(0).Count, Is.EqualTo(6));
+            Assert.That(_completeList.ToTailSegment(6).Count, Is.EqualTo(0));
+            Assert.That(_completeList.ToTailSegment(2).Count, Is.EqualTo(4));
+        }
+        
+        [Test]
+        public void when_updating_list()
+        {
+            _completeList.ToTailSegment(2)[0] = -1;
+            _completeList.ToTailSegment(2)[1] = -2;
+            
+            Assert.That(_completeList[2], Is.EqualTo(-1));
+            Assert.That(_completeList[3], Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void when_adding()
+        {
+            _completeList.ToTailSegment(3).Add(10);
+            Assert.That(_completeList[6], Is.EqualTo(10));
+        }
+        
+        [Test]
+        public void when_inserting()
+        {
+            _completeList.ToTailSegment(0).Insert(0,-1);
+            _completeList.ToTailSegment(_completeList.Count).Insert(0,-2);
+            _completeList.ToTailSegment(1).Insert(4, -3);
+            
+            CollectionAssert.AreEqual(_completeList, new[] { -1, 0, 1, 2, 3, -3, 4, 5, -2 });
+        }
+
+        [Test]
+        public void when_removing()
+        {
+            Assert.That(_completeList.ToTailSegment(0).Remove(0), Is.True);
+            Assert.That(_completeList.ToTailSegment(3).Remove(1), Is.False);
+            Assert.That(_completeList.ToTailSegment(3).Remove(4), Is.True);
+            CollectionAssert.AreEqual(_completeList, new[] { 1, 2, 3, 5 });
+        }
+
+        [Test]
+        public void when_inserting_range()
+        {
+            _completeList.ToTailSegment(0).InsertRange(1, new[] { -1, -2});
+            _completeList.ToTailSegment(_completeList.Count).InsertRange(0, new[] { -5, -6});
+            _completeList.ToTailSegment(_completeList.Count - 4 ).InsertRange(1, new[] { -3, -4});
+
+            CollectionAssert.AreEqual(_completeList, new[] { 0, -1, -2, 1, 2, 3, 4, -3, -4, 5, -5, -6 });
+        }
+
+        [Test]
+        public void when_removing_range()
+        {
+            _completeList.ToTailSegment(0).RemoveRange(0, 1);
+            _completeList.ToTailSegment(1).RemoveRange(1,2);
+
+            CollectionAssert.AreEqual(_completeList, new[] { 1, 2, 5 });
+        }
+    }
+}
---
+++ src/google-diff-match-patch/ListTailSegment.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiffMatchPatch
+{
+    internal static class ListTailSegment
+    {
+        public static ListTailSegment<T> ToTailSegment<T>(this List<T> list, int startAt) => new ListTailSegment<T>(list, startAt);
+    }
+
+    internal class ListTailSegment<T> : IList<T>
+    {
+        readonly List<T> _list;
+        readonly int _startAt;
+
+        public ListTailSegment(List<T> list, int startAt)
+        {
+            _list = list;
+            _startAt = startAt;
+        }
+
+        public IEnumerator<T> GetEnumerator() => _list.Skip(_startAt).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public void Clear()
+        {
+            _list.RemoveRange(_startAt, Count);
+        }
+
+        public bool Contains(T item) => _list.Skip(_startAt).Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _list.CopyTo(_startAt, array, arrayIndex, Count);
+        }
+
+        public bool Remove(T item)
+        {
+            for (var i = _startAt; i < _list.Count; i++)
+            {
+                var itemToTest = _list[i];
+                if (Equals(item, itemToTest))
+                {
+                    _list.RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int Count => _list.Count - _startAt;
+
+        public bool IsReadOnly => false;
+
+        public int IndexOf(T item)
+        {
+            for (var i = _startAt; i < _list.Count; i++)
+            {
+                var itemToTest = _list[i];
+                if (Equals(item, itemToTest))
+                {
+                    _list.RemoveAt(i);
+                    return i - _startAt;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            _list.Insert(index + _startAt, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            _list.RemoveAt(index + _startAt);
+        }
+
+        public void RemoveRange(int start, int count)
+        {
+            start = _startAt + start;
+            _list.RemoveRange(start, count);
+        }
+
+        public void InsertRange(int start, IEnumerable<T> collection)
+        {
+            start = _startAt + start;
+            _list.InsertRange(start, collection);
+        }
+
+        public T this[int index]
+        {
+            get => _list[index + _startAt];
+            set => _list[index + _startAt] = value;
+        }
+    }
+}
